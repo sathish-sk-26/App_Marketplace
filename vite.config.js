b0aaa0b22f7a7083f7cfd0f946245729c4f52df6
sathish-sk-26 <sathish.kumar@gohighlevel.com>
2026-05-05 22:22:48 +0530

    Setup GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
+  base: '/App_Marketplace/',
   plugins: [vue()],
   resolve: {
     alias: {
